f79e26d9435fe22b8c3763634ba76eb7f56a17ac
Pedro Botion <pedrobutii@gmail.com>
2024-08-02 08:44:11 -0300

    primeiro teste do codigo

--- automacao.py
+++ automacao.py
@@ -1,3 +1,25 @@
 #Código realizado para automatizar e facilitar a equipe de N1 na resposta de chamados na plataforma JIRA #
 
-print 
+# Texto de Bem vindo e apresenta as opções de indisponibilidade
+print ("Olá, seja bem-vindo ao programa de automação.")
+print ("1- Indisponibilidade de Energia")
+print ("2- Indisponibilidade de Link")
+print ("3- Indisponibilidade Interna")
+
+# Solicita uma resposta do usuário para as indisponibilidades
+resposta1 = print(input("Selecione:"))
+
+# Caso seja problema de energia
+if resposta1 == 1 :
+    resposta1_energia = print(input("O problema está relacionado com a Copel?",
+                                    "1- Sim",
+                                    "2- Não"))
+    
+if resposta1_energia == 1:
+    resposta2_a = print(input("Os colaboradores da unidade já entraram em contato com a Copel?",
+                                "1- Sim",
+                                "2- Não"))
+    
+# Caso seja problema de Link
+    if resposta1 == 2:
+        resposta1_link = print(input(""))
